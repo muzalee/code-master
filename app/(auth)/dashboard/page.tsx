27f3dbff178b40a1fe1216b13788d50238a6697f
muza <muzallyefah@gmail.com>
2023-11-09 19:49:18 +0800

    fix: header size

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -9,10 +9,10 @@ export default function Dashboard() {
        <section className="container flex flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-8 lg:py-20">
         <div className="flex flex-1 flex-col items-center gap-4 text-center lg:gap-8">
           <div className="space-y-4">
-            <h1 className="text-4xl font-bold lg:text-6xl">
+            <h1 className="text-2xl font-bold lg:text-3xl">
               {heroHeader.header}
             </h1>
-            <h2 className="text-lg font-light text-muted-foreground lg:text-3xl">
+            <h2 className="text-lg font-light text-muted-foreground lg:text-xl">
               {heroHeader.subheader}
             </h2>
           </div>
