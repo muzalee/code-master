92d891f3d9fb3d3f176b51feca2661ead5b0883c
muza <muzallyefah@gmail.com>
2023-11-09 23:47:58 +0800

    fix: show loading skeleton

--- components/user-detail.tsx
+++ components/user-detail.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react"
 import { getUser } from "@/lib/auth"
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function UserDetail() {
     const [user, setUser] = useState<User | null>(null);
@@ -27,17 +28,33 @@ export function UserDetail() {
         <CardDescription>Nice to see you again</CardDescription>
       </CardHeader>
       <CardContent className="grid gap-6">
-        <div className="flex items-center space-x-4">
-          <Avatar>
-            <AvatarImage src="/avatar.png" />
-            <AvatarFallback>AV</AvatarFallback>
-          </Avatar>
-          <div>
-            <p className="text-sm font-medium leading-none text-left">{ user?.name }</p>
-            <p className="text-sm text-muted-foreground text-left">{ user?.email }</p>
-          </div>
-        </div>
+        {isLoading ? (
+            <UserSkeleton />
+        ) : (
+            <div className="flex items-center space-x-4">
+                <Avatar>
+                    <AvatarImage src="/avatar.png" />
+                    <AvatarFallback>AV</AvatarFallback>
+                </Avatar>
+                <div>
+                    <p className="text-sm font-medium leading-none text-left">{ user?.name }</p>
+                    <p className="text-sm text-muted-foreground text-left">{ user?.email }</p>
+                </div>
+            </div>
+        )}
       </CardContent>
     </Card>
   )
 }
+
+function UserSkeleton() {
+    return (
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="space-y-2">
+          <Skeleton className="h-4 w-[250px]" />
+          <Skeleton className="h-4 w-[200px]" />
+        </div>
+      </div>
+    )
+  }
