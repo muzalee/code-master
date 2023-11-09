b21d46e38c7bc6323d5886c7a10c6ce3182a81f5
muza <muzallyefah@gmail.com>
2023-11-10 02:56:14 +0800

    fix: middleware handle assessment route

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { AssessmentCode } from './config/contents';
 
 export async function middleware(request: NextRequest) {
   const session = request.cookies.has('user');
   const pathname = request.nextUrl.pathname;
+  const url = new URL(request.nextUrl.href);
 
   if (session && ['/', '/login', '/register'].includes(pathname)) {
-    const url = request.nextUrl.clone();
     url.pathname = '/dashboard';
     return NextResponse.redirect(url);
-  } else if (!session && pathname === '/dashboard') {
-    const url = request.nextUrl.clone();
+  } else if (!session && ['/dashboard', '/assessment'].includes(pathname)) {
     url.pathname = '/';
     return NextResponse.redirect(url);
+  } else if (pathname === '/assessment') {
+    const url = request.nextUrl.clone();
+    const category = url.searchParams.get('category');
+  if (category && !Object.values(AssessmentCode).includes(category as AssessmentCode)) {
+      return NextResponse.json({ message: `Invalid category` }, { status: 400 });
+    }
   }
 
   return NextResponse.next()
