1c6cdf6f94de1ceb4d895f087af5d2213845e92d
muza <muzallyefah@gmail.com>
2023-11-09 23:14:53 +0800

    feat: display user details in dashboard

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -1,28 +1,16 @@
-import Link from "next/link"
-import { buttonVariants } from "@/components/ui/button"
-import { cn } from "@/lib/utils"
-import { heroHeader } from "@/config/contents"
+import { UserDetail } from "@/components/user-detail"
 
 export default function Dashboard() {
   return (
     <main>
-       <section className="container flex flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-8 lg:py-20">
-        <div className="flex flex-1 flex-col items-center gap-4 text-center lg:gap-8">
-          <div className="space-y-4">
-            <h1 className="text-2xl font-bold lg:text-3xl">
-              {heroHeader.header}
-            </h1>
-            <h2 className="text-lg font-light text-muted-foreground lg:text-xl">
-              {heroHeader.subheader}
-            </h2>
+      <section className="container flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-8 lg:py-20 lg:px-40">
+          <div className="grid gap-4 grid-cols-12">
+            <div className="col-span-6">
+              <UserDetail />
+            </div>
+            <div className="col-span-6">
+            </div>
           </div>
-          <Link
-            href="/assessment"
-            className={`w-[10rem] ${cn(buttonVariants({ size: "lg" }))}`}
-          >
-            New Assessment
-          </Link>
-        </div>
       </section>
     </main>
   )
---
+++ components/user-detail.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { User } from "@/lib/models/user"
+import { useEffect, useState } from "react"
+import { getUser } from "@/lib/auth"
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+
+export function UserDetail() {
+    const [user, setUser] = useState<User | null>(null);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        loadUser();
+    }, [])
+
+    async function loadUser() {
+        const u = await getUser();
+        setUser(u);
+        setLoading(false);
+    }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Welcome back!</CardTitle>
+      </CardHeader>
+      <CardContent className="grid gap-6">
+        <div className="flex items-center space-x-4">
+          <Avatar>
+            <AvatarImage src="/avatar.png" />
+            <AvatarFallback>AV</AvatarFallback>
+          </Avatar>
+          <div>
+            <p className="text-sm font-medium leading-none text-left">{ user?.name }</p>
+            <p className="text-sm text-muted-foreground text-left">{ user?.email }</p>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
